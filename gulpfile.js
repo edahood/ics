cd127a6a9c872d889918c1c48ee4fbe4da8c2433
eliot dahood <eliot@everyteams.com>
2018-06-10 20:31:11 -0400

    Fixed the clean and build commands

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,22 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
-
+var clean = require('gulp-clean');
 var paths = {
   src: 'src/**/*.js',
   dest: 'dist',
   test: 'test/**/*.js'
 };
+var clean = require('gulp-clean');
 
+gulp.task('clean-scripts', function () {
+    return gulp.src(paths.dest, {read: false})
+        .pipe(clean());
+});
 gulp.task('watch', function() {
   return gulp.watch(paths.src, ['build']);
 });
 
-gulp.task('build', function () {
+gulp.task('build', ['clean-scripts'], function () {
   return gulp.src(paths.src)
     .pipe(babel())
     .pipe(gulp.dest(paths.dest));
